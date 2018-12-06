b2e9f9528ca9cc44cfa36b623b55e7bad27c7fc2
schuyl3r <39696963+schuyl3r@users.noreply.github.com>
2018-12-06 20:04:26 +0800

    Add files via upload

---
+++ Grab/src/grab/Grab/src/grab/LoginController.java
@@ -0,0 +1,68 @@
+package grab;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class LoginController implements Initializable{
+
+    @FXML Button loginButton;
+    @FXML Button registerButton;
+    @FXML TextField mobileTextField;
+    @FXML TextField passwordTextField;
+    @FXML Label invalidityLabel;
+    
+    private static Main user;
+    
+    public static String mobile;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        user = new Main();
+    }
+    
+    public void moveToRegister (javafx.event.ActionEvent actionEvent) throws IOException {
+
+            FXMLLoader load = new FXMLLoader (getClass().getResource("Register.fxml"));
+            Parent fieldParent = load.load();
+            Scene scene = new Scene(fieldParent);
+            RegisterController controller = load.getController();
+            Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+    }
+
+    public void loggingIn (javafx.event.ActionEvent actionEvent) throws IOException {
+
+            mobile = mobileTextField.getText();
+            
+            if(user.checkAccount()){
+                System.out.println("Valid Account!");
+                
+                FXMLLoader load = new FXMLLoader (getClass().getResource("Map.fxml"));
+                Parent fieldParent = load.load();
+                Scene scene = new Scene(fieldParent);
+                MapController controller = load.getController();
+                Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+                window.setScene(scene);
+                window.show();
+                
+            } else {
+                System.out.println("Invalid Account!");
+                invalidityLabel.setVisible(true);
+                mobileTextField.setText("");
+            }
+    }
+}
+
---
+++ Grab/src/grab/Grab/src/grab/RideController.java
@@ -0,0 +1,50 @@
+package grab;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RideController {
+    @FXML Button acctButton;
+    @FXML Button historyButton;
+    @FXML Button mapButton;
+
+    public void moveToAccount (javafx.event.ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader load = new FXMLLoader (getClass().getResource("Account.fxml"));
+        Parent fieldParent = load.load();
+        Scene scene = new Scene(fieldParent);
+        AccountController controller = load.getController();
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void moveToHistory (javafx.event.ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader load = new FXMLLoader (getClass().getResource("History.fxml"));
+        Parent fieldParent = load.load();
+        Scene scene = new Scene(fieldParent);
+        HistoryController controller = load.getController();
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void moveToMap (javafx.event.ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader load = new FXMLLoader (getClass().getResource("Map.fxml"));
+        Parent fieldParent = load.load();
+        Scene scene = new Scene(fieldParent);
+        MapController controller = load.getController();
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+}
